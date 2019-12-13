be3310bab2689cd46717678ea21b8424adcbec8a
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2019-12-12 20:20:55 -0500

    mvp plus rps complete

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python
 
 import sys
+import functools
+
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+
+
+# @functools.lru_cache(maxsize=128)
+def eating_cookies(n, cache={}):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    if n not in cache:
+        cache[n] = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+    return cache[n]
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies),
+                                                                                    n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
